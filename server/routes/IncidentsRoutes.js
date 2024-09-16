16eecb0ed680ddd12bbcc2f6f694c87f59a7929a
Thabiso Mahlaola <2426592@students.wits.ac.za>
2024-09-16 21:06:43 +0200

    changes to incidents

--- server/routes/IncidentsRoutes.js
+++ server/routes/IncidentsRoutes.js
@@ -36,7 +36,6 @@ router.patch('/:id/status', async (req, res) => {
     try {
         const id_value = req.params.id;
         const {status} = req.body;
-        console.log(status);
         const UpdateStatus = await IncidentController.UpdateSafetyIncidents(id_value, status);
         res.status(200).json(UpdateStatus);
     } catch (err) {

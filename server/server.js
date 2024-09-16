04a2e94f9a0a2a6b42a8f0264385e1e918292d42
Luke Renton <luke.renton@icloud.com>
2024-09-17 01:40:29 +0200

    Server and yaml changes

--- server/server.js
+++ server/server.js
@@ -2,26 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const LoginRoutes = require('./routes/LoginRoutes');
-const SafetyResourcesRoutes = require('./routes/SafetyResourcesRoutes');
-const SafetyAlertsRoutes = require('./routes/SafetyAlertsRoutes');
-var deployment = true;
-app.use(express.json());
-const IncidentSRoutes = require('./routes/IncidentsRoutes');
 
-//body-parser
-app.use(express.json());
 // API route
 app.get('/api', (req, res) => {
     res.json({ message: 'Hello from the backend!' });
 });
-app.use('/users', LoginRoutes);
-app.use('/resources', SafetyResourcesRoutes); // Send any /resources requests to SafetyResourcesRoutes (e.g. /resources/safety-resources will be sent to SafetyResourcesRoutes)
-app.use('/alerts', SafetyAlertsRoutes);
-app.use('/incidents',IncidentSRoutes );
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Handle requests to main react page
 // NOTE: THIS ALL THE CODE BELOW HAS TO BE AT THE END OF THE FILE
@@ -31,7 +19,7 @@ reactRoutes = [
 ]
 // All other GET requests not handled before will return the React app
 app.get(reactRoutes, (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {

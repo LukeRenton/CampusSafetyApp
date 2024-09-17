03e108081a73137a4472e1b2e4a4cb33b91e6b20
Luke Renton <luke.renton@icloud.com>
2024-09-17 04:06:19 +0200

    reimplemnted endpoints

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 5000;
 
 // API route
 app.get('/api', (req, res) => {
@@ -9,7 +9,12 @@ app.get('/api', (req, res) => {
 });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use('/users', LoginRoutes);
+app.use('/resources', SafetyResourcesRoutes); // Send any /resources requests to SafetyResourcesRoutes (e.g. /resources/safety-resources will be sent to SafetyResourcesRoutes)
+app.use('/alerts', SafetyAlertsRoutes);
+app.use('/incidents',IncidentSRoutes);
 
 // Handle requests to main react page
 // NOTE: THIS ALL THE CODE BELOW HAS TO BE AT THE END OF THE FILE
@@ -19,7 +24,7 @@ reactRoutes = [
 ]
 // All other GET requests not handled before will return the React app
 app.get(reactRoutes, (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {

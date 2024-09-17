1c0f741818d6729408691747ea490ca5c008dba1
Luke Renton <luke.renton@icloud.com>
2024-09-17 03:40:43 +0200

    port change

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 8080;
 
 // API route
 app.get('/api', (req, res) => {

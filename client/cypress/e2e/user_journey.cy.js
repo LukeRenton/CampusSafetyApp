71f6e416a275f42ec8a23f32d910c10c18b5cc16
Luke Renton <luke.renton@icloud.com>
2024-09-29 13:43:29 +0200

    cypress update

--- client/cypress/e2e/user_journey.cy.js
+++ client/cypress/e2e/user_journey.cy.js
@@ -1,30 +1,33 @@
 // All our commands are found in the commands.js file. We can now use these commands in our tests.
 
 describe('Full User Journey', () => {
-    it('should allow a user to log in and interact with various features', () => {
-        // **Login Test**
-        cy.login('2540440', '123');
-    
-        // **Test Each Report**
-        // We can update the test case by just adding the different report types to the array
-        const reportTypes = ['medical', 'fire', 'natural', 'security', 'weather'];
-        cy.report(reportTypes);
+  it('should allow a user to log in and interact with various features', () => {
+      // **Login Test**
+      cy.login('2540440', '123');
+  
+      // **Test Each Report**
+      // We can update the test case by just adding the different report types to the array
+      const reportTypes = ['medical', 'fire', 'natural', 'security', 'weather'];
+      cy.report(reportTypes);
 
 
-        // **Test Each Incident**
-        cy.incident();
+      // **Test Each Incident**
+      cy.incident();
 
+      // **Test tabs in nav**
+      cy.checkTabs();
+      cy.get('.main-dark-back').click();
 
-        // **Test Walk Home Assist**
-        cy.walkHomeAssist();
-        cy.wait(5000);
-        cy.get('.main-dark-back').click();
-        cy.walkHomeAssistSchedule();
-        cy.wait(5000);
-        cy.get('.main-dark-back').click();
+      // **Test Walk Home Assist**
+      cy.walkHomeAssist();
+      cy.wait(5000);
+      cy.get('.main-dark-back').click();
+      cy.walkHomeAssistSchedule();
+      cy.wait(5000);
+      cy.get('.main-dark-back').click();
 
-        // **Test logout**
-        cy.logout();
-        
-      });
+      // **Test logout**
+      cy.logout();
+      
+    });
 });

7949e8a4e298dc20b54e8fc84f9e322485e9349a
MitchellNeilson <neilson.mitchell2win@gmail.com>
2024-09-29 21:14:28 +0200

    Added edit profile component

---
+++ client/src/components/EditMedicalProfile.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/EditMedicalProfile.css'
+
+export default function EditMedicalProfile() {
+  return (
+    <div className='edit-medical-profile-root'>
+        Stuff here
+    </div>
+  )
+}
